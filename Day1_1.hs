996e8a77dc7d8ed932dab4e4661c289f024ecd30
greenlightofforgiveness <119353123+greenlightofforgiveness@users.noreply.github.com>
2025-12-01 19:28:45 +0700

    Update Day1_1.hs

--- Day1_1.hs
+++ Day1_1.hs
@@ -14,6 +14,6 @@ count1 :: [String] -> (Int, Int) -> Int
 count1 [] (cnt, acc)       = cnt
 count1 (x : xs) (cnt, acc)  | head x == 'R' = let ans = ((acc + x') `mod` 100) in
 														if (ans == 0) then count1 xs (cnt + 1, ans) else count1 xs (cnt, ans) 
-              							| otherwise = let ans = ((acc - x') `mod` 100)
-              													in if (ans == 0) then count1 xs (cnt + 1, ans) else count1 xs (cnt, ans) 
-              								where x' = (read (tail x) :: Int)
+							| otherwise = let ans = ((acc - x') `mod` 100)
+													in if (ans == 0) then count1 xs (cnt + 1, ans) else count1 xs (cnt, ans) 
+								where x' = (read (tail x) :: Int)
