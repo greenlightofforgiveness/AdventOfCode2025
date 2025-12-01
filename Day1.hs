00614798588bb4111184afd8c7111b86a9c0aff4
greenlightofforgiveness <119353123+greenlightofforgiveness@users.noreply.github.com>
2025-12-01 21:08:45 +0700

    Update Day1.hs

--- Day1.hs
+++ Day1.hs
@@ -17,13 +17,13 @@ count1 (x : xs) (cnt, acc)  | head x == 'R' = let ans = ((acc + x') `mod` 100) i
 													in if (ans == 0) then count1 xs (cnt + 1, ans) else count1 xs (cnt, ans) 
 								where x' = (read (tail x) :: Int)
 
--- Idea: https://github.com/Tarmean/AOC2025/blob/main/src/Day1.hs
+-- Formula for Left case: https://github.com/Tarmean/AOC2025/blob/main/src/Day1.hs
 count2 :: [String] -> (Int, Int) -> Int
 count2 [] (cnt, acc)       = cnt
-count2 (x : xs) (cnt, acc)  | head x == 'R' = let ans = (acc + x') `mod` 100;
-												  cnt' = (acc + x') `div` 100
+count2 (x : xs) (cnt, acc)  | head x == 'R' = let ans = (acc + x') `mod` 100
+                                                  cnt' = (acc + x') `div` 100
 													in count2 xs (cnt + cnt', ans)
-							| otherwise = let ans = (acc - x') `mod` (100);
-											  cnt' = ((100 - acc) `mod` 100 + x') `div` 100
+							| otherwise     = let ans = (acc - x') `mod` 100
+							                      cnt' = ((100 - acc) `mod` 100 + x') `div` 100							
 													in count2 xs (cnt + cnt', ans)
 								where x' = (read (tail x) :: Int)
