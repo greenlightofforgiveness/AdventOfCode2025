d01c32ffc80be8e585f74b71f3f4d5945a0b442d
greenlightofforgiveness <119353123+greenlightofforgiveness@users.noreply.github.com>
2025-12-03 21:17:08 +0700

    Update Day3_2.hs

--- Day3_2.hs
+++ Day3_2.hs
@@ -2,7 +2,7 @@ module Main (main) where
 import Data.Functor ((<$>))
 import Data.List (find, singleton)
 import qualified Data.Map as Map
-import Data.Maybe
+import Data.Maybe (fromJust)
 import System.IO
    
 main = do  
@@ -16,19 +16,20 @@ count2 :: [String] -> Int -> Int
 count2 [] acc            = acc
 count2 (x : xs) acc      = count2 xs (acc + acc')
                                         where   l = length x - 1
-                                                m = Map.fromListWith (++) (zip x (map singleton [0 .. l]))
+                                                m' = Map.fromListWith (++) (zip x (map singleton [0 .. l]))
+                                                m = Map.map reverse m'
                                                 k = reverse (Map.keys m)
                                                 acc' | length k == 1 = read (take 12 (repeat (x !! 0))) :: Int  
                                                      | otherwise = ans
                                                                         where maxIndex = l + 1 - 12
-                                                                              x1 = find (\t -> any (<= maxIndex) (reverse (m Map.! t))) k
-                                                                              i = fromJust (find (<= maxIndex) (reverse (m Map.! (fromJust x1))))
-                                                                              ans = helper m k l i 11 (read ([fromJust x1]) :: Int)
+                                                                              x1 = find (\t -> any (<= maxIndex) (m Map.! t)) k
+                                                                              minIndex = fromJust (find (<= maxIndex) (m Map.! (fromJust x1)))
+                                                                              ans = helper m k l minIndex 11 (read ([fromJust x1]) :: Int)
 helper :: Map.Map Char [Int] -> String -> Int -> Int -> Int -> Int -> Int
-helper m k l i 0 ans = ans
-helper m k l i cnt ans = helper m k l i' (cnt - 1) ans'
-                        where ans' = ans * 10 + x
-                              maxIndex = l + 1 - cnt
-                              x' = find (\t -> (any (\x -> (x > i) && (x <= maxIndex)) (reverse (m Map.! t)))) k
-                              x = read ([fromJust x']) :: Int
-                              i' = fromJust (find (\x -> (x > i) && (x <= maxIndex))(reverse (m Map.! (fromJust x'))))
+helper m k l minIndex 0 ans = ans
+helper m k l minIndex cnt ans = helper m k l minIndex' (cnt - 1) ans'
+                                        where ans' = ans * 10 + x
+                                              maxIndex = l + 1 - cnt
+                                              x' = find (\t -> (any (\x -> (x > minIndex) && (x <= maxIndex)) (m Map.! t))) k
+                                              x = read ([fromJust x']) :: Int
+                                              minIndex' = fromJust (find (\x -> (x > minIndex) && (x <= maxIndex)) (m Map.! (fromJust x')))
