7ec25b55152c275d980d1eb277e912230c51e81e
greenlightofforgiveness <119353123+greenlightofforgiveness@users.noreply.github.com>
2025-12-05 23:43:54 +0700

    Update Day5_1.hs

--- Day5_1.hs
+++ Day5_1.hs
@@ -1,23 +1,13 @@
 module Main (main) where
 import Data.Functor ((<$>))
-import Data.List.Split
-import Data.List (notElem)
+import Data.List.Split (splitOn)
 import System.IO
-
-type Graph a = [(a, [a])]
             
 main = do  
     contents <- readFile "input.txt"
     putStrLn $ analyze1 (lines contents)
 
 analyze1 :: [String] -> String
-analyze1 s = show $ helper d ingr 0
+analyze1 s = show $ length $ filter (== True) $ map (\x -> any (\(a, b) -> ((x >= a) && (x <= b))) d) ingr
                 where d = concatMap (\x -> let [a, b] = splitOn ("-") x in [(read a :: Int, read b :: Int)] ) $ takeWhile (/= "") s
-                      ingr = tail $ dropWhile (/= "") s
-
-
-helper d [] acc        = acc
-helper d (x  : xs) acc = helper d xs acc'
-                                where   x' = read x :: Int
-                                        acc' | any (\(a, b) -> if ((x' >= a) && (x' <= b)) then True else False) d = acc + 1
-                                             | otherwise = acc
+                      ingr = map (\x -> read x :: Int) $ tail $ dropWhile (/= "") s
