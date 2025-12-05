0dbfab453e730182a6afc7d5ebb74433b985d230
greenlightofforgiveness <119353123+greenlightofforgiveness@users.noreply.github.com>
2025-12-06 00:04:04 +0700

    Create Day5_2.hs

---
+++ Day5_2.hs
@@ -0,0 +1,22 @@
+module Main (main) where
+import Data.Functor ((<$>))
+import Data.List.Split (splitOn)
+import Data.List (sort)
+import System.IO
+            
+main = do  
+    contents <- readFile "input.txt"
+    putStrLn $ analyze2 (lines contents)
+
+analyze2 :: [String] -> String
+analyze2 s = show $ helper (sort d) (-1) 0
+                where d = concatMap (\x -> let [a, b] = splitOn ("-") x in [(read a :: Int, read b :: Int)] ) $ takeWhile (/= "") s
+                      
+-- Idea: https://aoc-puzzle-solver.streamlit.app/
+
+helper :: [(Int, Int)] -> Int -> Int -> Int
+helper [] prevUpper cnt              = cnt
+helper ((a, b) : xs) prevUpper cnt  | b <= prevUpper = helper xs prevUpper cnt
+                                    | otherwise = helper xs prevUpper' cnt'
+                                                where cnt' = cnt + b - (max a (prevUpper + 1)) + 1
+                                                      prevUpper' = max b prevUpper
