88d7f27993200898fb471123d5a9e502782a3b94
greenlightofforgiveness <119353123+greenlightofforgiveness@users.noreply.github.com>
2025-12-06 17:16:31 +0700

    Create Day6_2.hs

---
+++ Day6_2.hs
@@ -0,0 +1,22 @@
+module Main (main) where
+import Data.Functor ((<$>))
+import Data.List.Split (splitWhen)
+import Data.List (transpose, init)
+import Data.Char (isDigit)
+import System.IO
+            
+main = do  
+    contents <- readFile "input.txt"
+    putStrLn $ analyze (lines contents)
+
+analyze :: [String] -> String
+analyze s = show $ helper numbs m 0
+                        where numbs = map (map (\x -> read x :: Int)) $ splitWhen (all (== ' ')) $ transpose (init s)
+                              m = filter (/= ' ' ) (last s)
+                              l = length s - 1
+
+helper :: [[Int]] -> String -> Int -> Int                              
+helper [] _ acc = acc
+helper (x : xs) (m : ms) acc = helper xs ms (acc + acc')
+                                        where acc' | m == '*' = foldl (*) 1 x
+                                                   | otherwise = foldl (+) 0 x
