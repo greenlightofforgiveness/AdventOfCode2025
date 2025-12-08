2256679af94fd6d321ba1e3cf2c9bdd82c6e0df0
greenlightofforgiveness <119353123+greenlightofforgiveness@users.noreply.github.com>
2025-12-08 22:54:44 +0700

    Update Day8_1.hs

--- Day8_1.hs
+++ Day8_1.hs
@@ -17,6 +17,7 @@ analyze s = show $ (ans !! 0) * (ans !! 1) * (ans !! 2)
                       dist = map (\((x1, y1, z1), (x2, y2, z2)) -> let d = sqrt((x1 - x2)^2 + (y1 - y2)^2 + (z1 - z2)^2) in ((x1, y1, z1), (x2, y2, z2), d)) pairs
                       ans = helper (sortBy (\(_, _, d1) (_, _, d2) -> compare d1 d2) dist) [] 0
                       
+helper :: [((Double, Double, Double), (Double, Double, Double), Double)] -> [Set.Set (Double, Double, Double)] -> Int -> [Int]
 helper dist cs 1000 = reverse $ sort $ map Set.size cs
 helper ((p1, p2, d) : ds) cs i = helper ds cs' (i + 1)
                                         where cs1 = find (Set.member p1) cs
