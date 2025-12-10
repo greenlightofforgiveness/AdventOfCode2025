ea777bd287746cafbd0810186dfb05958896acff
greenlightofforgiveness <119353123+greenlightofforgiveness@users.noreply.github.com>
2025-12-10 22:49:14 +0700

    Update Day9_1.hs

--- Day9_1.hs
+++ Day9_1.hs
@@ -11,4 +11,4 @@ analyze :: [String] -> String
 analyze s = show $ maximum areas
                 where s' = map (\t -> let [c, r] = splitOn "," t in (read r :: Int, read c :: Int)) s
                       pairs = [ ((r1, c1), (r2, c2)) | (r1, c1) <- s', (r2, c2) <- s', c1 <= c2]
-                      areas = map (\((r1, c1), (r2, c2)) -> (abs (r2 - r1 + 1)) * (abs (c2 - c1 + 1))) pairs
+                      areas = map (\((r1, c1), (r2, c2)) -> (abs (r2 - r1) + 1) * (abs (c2 - c1) + 1)) pairs
